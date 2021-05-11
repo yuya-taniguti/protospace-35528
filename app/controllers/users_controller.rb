c678039ddbd5573b3be6466c2481e6c44e9a24ec
yuya-taniguti <yuya2624@gmail.com>
2021-05-11 11:43:45 +0900

    修正２回目

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
-  private
-  def user_params
-    params.require(:user).permit(:name, :profile, :occupation, :position).merge(user_id: current_user.id)
-  end
 end
